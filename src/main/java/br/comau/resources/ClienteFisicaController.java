db003ae02e4c8409324a6f325f6e09642e637795
Jose RF Junior <web2ajax@gmail.com>
2020-07-10 11:36:32 -0400

    update arquitetura

--- src/main/java/br/comau/resources/ClienteFisicaController.java
+++ src/main/java/br/comau/resources/ClienteFisicaController.java
@@ -7,18 +7,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.comau.domain.ClienteFisica;
 import br.comau.dto.ClienteFisicaDTO;
 import br.comau.services.ClienteFisicaService;
 
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/api")
 public class ClienteFisicaController {
---
+++ src/test/java/br/comau/resources/ClienteFisicaControllerTest.java
@@ -0,0 +1,28 @@
+package br.comau.resources;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+class ClienteFisicaControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void findAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void delete() {
+    }
+}
