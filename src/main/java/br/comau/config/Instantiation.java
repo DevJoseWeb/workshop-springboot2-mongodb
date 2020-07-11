627ce1a68e568b195418d15bb786628c4224148a
Jose RF Junior <web2ajax@gmail.com>
2020-07-10 22:07:52 -0400

    update documents

--- src/main/java/br/comau/config/Instantiation.java
+++ src/main/java/br/comau/config/Instantiation.java
@@ -17,14 +17,18 @@ public class Instantiation implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-		ClienteFisica herick = new ClienteFisica(null,"Herick Marques", "64525430249", "Aluno");
-		ClienteFisica fred = new ClienteFisica(null,"Frederico Motha", "64525430249", "Aluno");
-		ClienteFisica lou = new ClienteFisica(null,"Lourranio Simoes", "64525430249", "Aluno");
-
+		ClienteFisica herick = new ClienteFisica(null,"Herick Marques", "64525430249", "Aluno", "Rua dos Java Lang", "+55 91254360", "web2ajax@gmail.com", "Ativo", "Goolgolplex", "02/02/2020");
+		ClienteFisica lourranio = new ClienteFisica(null,"Lourranio Simoes Marques", "64525430249", "Aluno", "Rua dos Java Lang", "+55 91254360", "web2ajax@gmail.com", "Ativo", "Goolgolplex", "02/02/2020");
+		ClienteFisica fred = new ClienteFisica(null,"Fred Marques", "64525430249", "Aluno", "Rua dos Java Lang", "+55 91254360", "web2ajax@gmail.com", "Ativo", "Goolgolplex", "02/02/2020");
+		ClienteFisica maria = new ClienteFisica(null,"Maria Avila Toledo", "64525430249", "Aluno", "Rua dos Java Lang", "+55 91254360", "web2ajax@gmail.com", "Ativo", "Goolgolplex", "02/02/2020");
+		ClienteFisica pedro = new ClienteFisica(null,"Pedro Marques", "64525430249", "Aluno", "Rua dos Java Lang", "+55 91254360", "web2ajax@gmail.com", "Ativo", "Goolgolplex", "02/02/2020");
+		ClienteFisica joao = new ClienteFisica(null,"Joao Simoes Marques", "64525430249", "Aluno", "Rua dos Java Lang", "+55 91254360", "web2ajax@gmail.com", "Ativo", "Goolgolplex", "02/02/2020");
+		ClienteFisica ze = new ClienteFisica(null,"Ze Marques", "64525430249", "Aluno", "Rua dos Java Lang", "+55 91254360", "web2ajax@gmail.com", "Ativo", "Goolgolplex", "02/02/2020");
+		ClienteFisica fernandes = new ClienteFisica(null,"Fred Avila Toledo", "64525430249", "Aluno", "Rua dos Java Lang", "+55 91254360", "web2ajax@gmail.com", "Ativo", "Goolgolplex", "02/02/2020");
 	}
 
 }
